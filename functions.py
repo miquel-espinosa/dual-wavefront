bd290ca9bf826c31d0a98c0600a4fd191d1970ce
MiquelEspinosa <mespinosami@gmail.com>
2022-01-11 17:49:57 +0100

    improved plots

--- functions.py
+++ functions.py
@@ -1,5 +1,8 @@
 import numpy as np
 import tkinter as tk
+import matplotlib.pyplot as plt
+import math
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def read_file(txt):
     with open(txt) as file:
@@ -41,3 +44,56 @@ def get_screen_dimensions():
     width = int(geometry.split('x')[0])
     height = int(geometry.split('x')[1].split('+')[0])
     return width/dpi, height/dpi
+
+
+
+def get_plots():
+    
+    fig, axd = plt.subplot_mosaic([['ax_map', 'ax_dist_s'],
+                               ['ax_map', 'ax_dist_g']])
+    
+    fig.tight_layout()
+
+    fig.subplots_adjust(bottom=0.03, top=0.97, left=0.03, right=0.97)
+    fig.subplots_adjust(wspace=0.1, hspace=0.1)
+
+    width_in, height_in = get_screen_dimensions()
+    fig.set_size_inches(round(width_in)-1,round(height_in)-1)
+
+    return fig, (axd['ax_map'], axd['ax_dist_s'], axd['ax_dist_g'])
+
+def gradient_plot(fig, ax_dist, map, dist_transform, dist_transform_obstacles, point, color):
+    horizontal_min, horizontal_max, horizontal_stepsize = 0, 23, 1
+    vertical_min, vertical_max, vertical_stepsize = 0, 10, 1
+
+    horizontal_dist = horizontal_max-horizontal_min
+    vertical_dist = vertical_max-vertical_min
+
+    horizontal_stepsize = horizontal_dist / float(math.ceil(horizontal_dist/float(horizontal_stepsize)))
+    vertical_stepsize = vertical_dist / float(math.ceil(vertical_dist/float(vertical_stepsize)))
+
+    xv, yv = np.meshgrid(np.arange(horizontal_min, horizontal_max, horizontal_stepsize),
+                        np.arange(vertical_min, vertical_max, vertical_stepsize))
+
+    xv+=horizontal_stepsize/2.0
+    yv+=vertical_stepsize/2.0
+
+
+    yd, xd = np.gradient(dist_transform)
+
+    def func_to_vectorize(x, y, dx, dy, map, ax_dist, scaling=0.2):
+        ax_dist.arrow(x, y, dx*scaling*map, dy*scaling*map, fc="k", ec="k", head_width=0.12, head_length=0.2)
+
+    vectorized_arrow_drawing = np.vectorize(func_to_vectorize)
+
+    vectorized_arrow_drawing(xv, yv, xd, yd, map, ax_dist, 0.2)
+
+    divider = make_axes_locatable(ax_dist)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
+
+    im = ax_dist.imshow(np.flip(dist_transform_obstacles,0), extent=[horizontal_min, horizontal_max, vertical_min, vertical_max])
+
+    ax_dist.scatter(point[0]+0.5,point[1]+0.5, c=color, s=200, zorder=100, edgecolors='black')
+
+
+    fig.colorbar(im, cax=cax, orientation='vertical')
--- main.py
+++ main.py
@@ -5,71 +5,40 @@ import math
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
-from functions import read_file, get_screen_dimensions
+from functions import get_plots, gradient_plot, read_file
 
 start, goal, map, obstacles, dist_s, dist_g = read_file("input.txt")
 
 
 
 dist_transform_s = ndimage.distance_transform_edt(dist_s)
-max_value = np.max(dist_transform_s)
-dist_transform_s_obstacles = dist_transform_s+obstacles*max_value
+dist_transform_g = ndimage.distance_transform_edt(dist_g)
+max_value_s = np.max(dist_transform_s)
+max_value_g = np.max(dist_transform_g)
+dist_transform_s_obstacles = dist_transform_s+obstacles*max_value_s
+dist_transform_g_obstacles = dist_transform_g+obstacles*max_value_g
+
+
 
 # Define plots
-fig, (ax_map, ax_dist) = plt.subplots(1,2)
-fig.tight_layout()
-
-fig.subplots_adjust(bottom=0.03, top=0.97, left=0.03, right=0.97)
-fig.subplots_adjust(wspace=0.1, hspace=0.1)
-
-width_in, height_in = get_screen_dimensions()
-fig.set_size_inches(round(width_in)-1,round(height_in)-1)
+fig, (ax_map, ax_dist_s, ax_dist_g) = get_plots()
 
 # Map (black and white)
 ax_map.imshow(map, cmap=plt.cm.gray, origin='lower')
 
-ax_map.scatter(*start, c='r', s=100)
-ax_map.annotate('START', xy=(start[0]-0.3,start[1]+0.3))
-ax_map.scatter(*goal, c='b', s=100)
-ax_map.annotate('GOAL', xy=(goal[0]-0.3,goal[1]+0.3))
-
-horizontal_min, horizontal_max, horizontal_stepsize = 0, 23, 1
-vertical_min, vertical_max, vertical_stepsize = 0, 10, 1
-
-horizontal_dist = horizontal_max-horizontal_min
-vertical_dist = vertical_max-vertical_min
-
-horizontal_stepsize = horizontal_dist / float(math.ceil(horizontal_dist/float(horizontal_stepsize)))
-vertical_stepsize = vertical_dist / float(math.ceil(vertical_dist/float(vertical_stepsize)))
-
-xv, yv = np.meshgrid(np.arange(horizontal_min, horizontal_max, horizontal_stepsize),
-                     np.arange(vertical_min, vertical_max, vertical_stepsize))
-
-xv+=horizontal_stepsize/2.0
-yv+=vertical_stepsize/2.0
+ax_map.scatter(*start, c='r', s=200, edgecolors='black')
+ax_map.annotate('START', xy=(start[0]-0.5,start[1]+0.5))
+ax_map.scatter(*goal, c='lime', s=200, edgecolors='black')
+ax_map.annotate('GOAL', xy=(goal[0]-0.5,goal[1]+0.5))
 
 
-yd, xd = np.gradient(dist_transform_s)
+gradient_plot(fig,ax_dist_s,map,dist_transform_s,dist_transform_s_obstacles, start, 'r')
+gradient_plot(fig,ax_dist_g,map,dist_transform_g,dist_transform_g_obstacles, goal, 'lime')
 
-def func_to_vectorize(x, y, dx, dy, map, scaling=0.2):
-    plt.arrow(x, y, dx*scaling*map, dy*scaling*map, fc="k", ec="k", head_width=0.12, head_length=0.2)
-
-vectorized_arrow_drawing = np.vectorize(func_to_vectorize)
-
-im = ax_dist.imshow(np.flip(dist_transform_s_obstacles,0), extent=[horizontal_min, horizontal_max, vertical_min, vertical_max])
-vectorized_arrow_drawing(xv, yv, xd, yd, map, 0.2)
-
-divider = make_axes_locatable(ax_dist)
-cax = divider.append_axes('right', size='5%', pad=0.05)
-
-fig.colorbar(im, cax=cax, orientation='vertical')
+ax_map.axis('off')
+ax_dist_s.axis('off')
+ax_dist_g.axis('off')
 
 
-
-# img_dist = ax_dist.imshow(dist)
-
-# cbar = fig.colorbar(img_dist, ax=ax_dist)
-
-# ax_dist.axis('off')
 plt.show()
 
