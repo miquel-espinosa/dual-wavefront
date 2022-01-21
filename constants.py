ae0be382432ba5ad8dde0083b24fcc13f5d420a3
MiquelEspinosa <mespinosami@gmail.com>
2022-01-21 17:00:04 +0100

    more gifs

--- constants.py
+++ constants.py
@@ -1,15 +1,15 @@
 # Constants for animations
 VIDEO = True
 ANIMATION = True
-TREE_EXTENSION_ANIMATION = False
+TREE_EXTENSION_ANIMATION = True
 
 # Constant for gradient plots
 GRADIENT_PLOTS = False
 
 # Constants for saving videos and plots
-VIDEO_NAME = 'videos/holes'
-OUTPUT_FILE_NAME = 'images/holes'
-INPUT_FILE_NAME = 'maps/holes.txt'
+VIDEO_NAME = 'videos/simple-tree'
+OUTPUT_FILE_NAME = 'images/simple-tree'
+INPUT_FILE_NAME = 'maps/simple.txt'
 
 # Enable disable diagonal navigation
 DIAGONALS = False # TODO: diagonals not working yet
